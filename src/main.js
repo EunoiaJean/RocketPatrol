b5bf7a0d05f28662ed60d49bb9557a5f269be2d7
EunoiaJean <56204918+EunoiaJean@users.noreply.github.com>
2020-04-08 15:47:16 -0700

    Scenes & Assets implemented

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     Height: 480,
+    scene: [ Menu, Play ],
 };
 let game = new Phaser.Game(config);
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,5 +1,12 @@
 class Menu extends Phaser.Scene {
     constructor() {
-        super("menuScene")
+        super("menuScene");
+    }
+    create() {
+        console.log(this);
+        this.add.text(20, 20, "Rocket Patrol Menu");
+        
+        //launch the next scene
+        this.scene.start("playScene");
     }
 }
